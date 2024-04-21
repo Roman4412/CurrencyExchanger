51d9e8bc134e4fbcb89e46e0bcd874655776cf00
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-04-21 07:29:01 +0300

    fix:    - the currency name can now include whitespaces

--- src/main/java/com/projects/study/constant/ValidatorKit.java
+++ src/main/java/com/projects/study/constant/ValidatorKit.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public final class ValidatorKit {
     public static final String CUR_CODE_REGEX = "^[a-zA-Z]{3}$";
-    public static final String CUR_NAME_REGEX = "^[a-zA-Z]{3,100}$";
+    public static final String CUR_NAME_REGEX = "^^[a-zA-Z\\s]{3,100}$";
     public static final String CUR_SIGN_REGEX = "^[a-zA-Z]{1,2}$";
     public static final String ER_CODE_REGEX = "^[a-zA-Z]{6}$";
     public static final String ER_RATE_REGEX = "^\\d+(?:[\\.,]\\d{0,4})?$";
